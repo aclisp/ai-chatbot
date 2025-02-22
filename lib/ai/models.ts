0d4006ce0b58ff5c6c6b8970ffe4c6cae835bc3f
huanghao <huanghao@youxin.cloud>
2025-02-22 22:04:09 +0800

    Log fetch to model provider

--- lib/ai/models.ts
+++ lib/ai/models.ts
@@ -6,10 +6,34 @@ import {
   wrapLanguageModel,
 } from 'ai';
 
+type FetchFunction = typeof globalThis.fetch;
+const logFetch: FetchFunction = async (url, options): Promise<Response> => {
+  // @ts-ignore
+  const { method = 'GET', headers = {}, body = null } = options;
+  console.log(`==> ${method} ${url}`);
+  console.log('==> (Headers) %o', headers);
+
+  if (body) {
+    if (
+      typeof body === 'string' &&
+      headers['Content-Type'] === 'application/json'
+    ) {
+      console.log('==> (Body) %o', JSON.parse(body));
+    } else {
+      console.log('==> (Body) %o', body);
+    }
+  }
+
+  const response = await fetch(url, options);
+  console.log(`<== ${response.status} ${response.statusText}`);
+  return response;
+};
+
 const ali = createOpenAICompatible({
   name: 'Alibaba Cloud',
   apiKey: process.env.ALI_API_KEY,
   baseURL: 'https://dashscope.aliyuncs.com/compatible-mode/v1',
+  fetch: logFetch,
 });
 
 export const DEFAULT_CHAT_MODEL: string = 'chat-model-small';
